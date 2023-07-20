e47bcae7b1efdf889d61eaca0bc5e56d6a6f71e3
xacah-x <xacahmytl@gmail.com>
2023-07-21 01:49:53 +0300

    added more scenario query

--- SQL/SQLHomeWork1.sql
+++ SQL/SQLHomeWork1.sql
@@ -8,3 +8,9 @@ select * from film Where rental_rate = 0.99 And replacement_cost = 12.99 OR repl
 select last_name,first_name From customer where first_name = 'Mary'
 --film tablosundaki uzunluğu(length) 50 ten büyük OLMAYIP aynı zamanda rental_rate değeri 2.99 veya 4.99 OLMAYAN verileri sıralayınız.
 Select * from film where length <=50 and rental_rate Not in (2.99,4.99);
+--film tablosunda bulunan tüm sütunlardaki verileri replacement cost değeri 12.99 dan büyük eşit ve 16.99 küçük olma koşuluyla sıralayınız ( BETWEEN - AND yapısını kullanınız.)
+select * from film where  (replacement_cost >= 12.99) and (replacement_cost <=16.99)
+--actor tablosunda bulunan first_name ve last_name sütunlardaki verileri first_name 'Penelope' veya 'Nick' veya 'Ed' değerleri olması koşuluyla sıralayınız. ( IN operatörünü kullanınız.)
+SELECT first_name,last_name FROM actor WHERE first_name IN ('Penelope','Nick','Ed');
+--film tablosunda bulunan tüm sütunlardaki verileri rental_rate 0.99, 2.99, 4.99 VE replacement_cost 12.99, 15.99, 28.99 olma koşullarıyla sıralayınız. ( IN operatörünü kullanınız.)
+select * from film where rental_rate IN (0.99,2.99,4.99) and replacement_cost IN (12.99,15.99,28.99)
