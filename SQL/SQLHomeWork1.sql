0783af5716b5630c62949b9dad9917f660df7c99
xacah-x <xacahmytl@gmail.com>
2023-07-21 02:18:57 +0300

    added more scenario query

--- SQL/SQLHomeWork1.sql
+++ SQL/SQLHomeWork1.sql
@@ -14,3 +14,11 @@ select * from film where  (replacement_cost >= 12.99) and (replacement_cost <=16
 SELECT first_name,last_name FROM actor WHERE first_name IN ('Penelope','Nick','Ed');
 --film tablosunda bulunan tüm sütunlardaki verileri rental_rate 0.99, 2.99, 4.99 VE replacement_cost 12.99, 15.99, 28.99 olma koşullarıyla sıralayınız. ( IN operatörünü kullanınız.)
 select * from film where rental_rate IN (0.99,2.99,4.99) and replacement_cost IN (12.99,15.99,28.99)
+--country tablosunda bulunan country sütunundaki ülke isimlerinden 'A' karakteri ile başlayıp 'a' karakteri ile sonlananları sıralayınız.
+select country from country where country like 'A_%a'
+--country tablosunda bulunan country sütunundaki ülke isimlerinden en az 6 karakterden oluşan ve sonu 'n' karakteri ile sonlananları sıralayınız.
+SELECT country FROM country WHERE LENGTH(country) >= 6 AND country LIKE '%n';
+--film tablosunda bulunan title sütunundaki film isimlerinden en az 4 adet büyük ya da küçük harf farketmesizin 'T' karakteri içeren film isimlerini sıralayınız.
+SELECT title FROM film WHERE title ILIKE '%T%T%T%T%';
+--film tablosunda bulunan tüm sütunlardaki verilerden title 'C' karakteri ile başlayan ve uzunluğu (length) 90 dan büyük olan ve rental_rate 2.99 olan verileri sıralayınız.
+SELECT * FROM film WHERE title LIKE 'C%' AND length > 90 AND rental_rate = 2.99;
