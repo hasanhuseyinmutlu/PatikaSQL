6ea2d8f5c6d984331d362dc6ec5f72af96608812
xacah-x <xacahmytl@gmail.com>
2023-07-21 21:28:18 +0300

    homework 12 queries

--- SQL/SQLHomeWork1.sql
+++ SQL/SQLHomeWork1.sql
@@ -132,3 +132,22 @@ SELECT first_name FROM actor
 EXCEPT
 SELECT first_name FROM customer;
 --İlk 3 sorguyu tekrar eden veriler için de yapalım. Yapıldı
+--->
+--film tablosunda film uzunluğu length sütununda gösterilmektedir. Uzunluğu ortalama film uzunluğundan fazla kaç tane film vardır?
+SELECT COUNT(*) 
+FROM film
+WHERE length > (SELECT AVG(length) FROM film);
+--film tablosunda en yüksek rental_rate değerine sahip kaç tane film vardır?
+SELECT COUNT(*) 
+FROM film
+WHERE rental_rate = (SELECT MAX(rental_rate) FROM film);
+--film tablosunda en düşük rental_rate ve en düşün replacement_cost değerlerine sahip filmleri sıralayınız
+SELECT *
+FROM film
+WHERE rental_rate = (SELECT MIN(rental_rate) FROM film)
+AND replacement_cost = (SELECT MIN(replacement_cost) FROM film);
+--payment tablosunda en fazla sayıda alışveriş yapan müşterileri(customer) sıralayınız.
+SELECT customer_id, COUNT(*) 
+FROM payment
+GROUP BY customer_id
+ORDER BY customer_id DESC;
