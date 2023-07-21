8d786b973f522be51001c4460f45959a3f2e85ca
xacah-x <xacahmytl@gmail.com>
2023-07-21 18:20:21 +0300

    patika home work 9 queries added

--- SQL/SQLHomeWork1.sql
+++ SQL/SQLHomeWork1.sql
@@ -98,3 +98,11 @@ DELETE FROM employee
 WHERE name = 'Michael Brown' AND birthday = '1986-05-20';
 DELETE FROM employee
 WHERE email = 'emily.brown@example.org' AND birthday = '1993-02-14';
+--city tablosu ile country tablosunda bulunan şehir (city) ve ülke (country) isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
+SELECT city.city, country.country FROM city INNER JOIN country ON city.country_id = country.country_id;
+--customer tablosu ile payment tablosunda bulunan payment_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
+SELECT payment.payment_id, customer.first_name, customer.last_name FROM payment INNER JOIN customer ON payment.customer_id = customer.customer_id;
+--customer tablosu ile rental tablosunda bulunan rental_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
+SELECT rental.rental_id, customer.first_name, customer.last_name FROM rental INNER JOIN customer ON rental.customer_id = customer.customer_id;
+
+
