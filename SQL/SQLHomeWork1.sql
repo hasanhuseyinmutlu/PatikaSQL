7d26cb9faa2347b8eec091712992ab03981fe962
xacah-x <xacahmytl@gmail.com>
2023-07-21 14:31:00 +0300

    homework 4 query added

--- SQL/SQLHomeWork1.sql
+++ SQL/SQLHomeWork1.sql
@@ -22,3 +22,13 @@ SELECT country FROM country WHERE LENGTH(country) >= 6 AND country LIKE '%n';
 SELECT title FROM film WHERE title ILIKE '%T%T%T%T%';
 --film tablosunda bulunan tüm sütunlardaki verilerden title 'C' karakteri ile başlayan ve uzunluğu (length) 90 dan büyük olan ve rental_rate 2.99 olan verileri sıralayınız.
 SELECT * FROM film WHERE title LIKE 'C%' AND length > 90 AND rental_rate = 2.99;
+--film tablosunda bulunan replacement_cost sütununda bulunan birbirinden farklı değerleri sıralayınız.
+SELECT DISTINCT replacement_cost FROM film
+--film tablosunda bulunan replacement_cost sütununda birbirinden farklı kaç tane veri vardır?
+SELECT DISTINCT COUNT(replacement_cost) FROM film
+--film tablosunda bulunan film isimlerinde (title) kaç tanesini T karakteri ile başlar ve aynı zamanda rating 'G' ye eşittir?
+SELECT COUNT(*) FROM film WHERE title LIKE 'T%' AND rating= 'G';
+--country tablosunda bulunan ülke isimlerinden (country) kaç tanesi 5 karakterden oluşmaktadır?
+SELECT country from country WHERE length(country) = 5
+--city tablosundaki şehir isimlerinin kaç tanesi 'R' veya r karakteri ile biter?
+SELECT COUNT(*) FROM city WHERE city ILIKE '%R';
