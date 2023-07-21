9f09f6bf1ea6cceda4792e953e0d5e9534bf2643
xacah-x <xacahmytl@gmail.com>
2023-07-21 16:34:42 +0300

    new queries

--- SQL/SQLHomeWork1.sql
+++ SQL/SQLHomeWork1.sql
@@ -46,3 +46,11 @@ select count(*) from film where title like 'C%'
 select   max(length) from film  where  rental_rate = 0.99
 --film tablosunda bulunan filmlerin uzunluğu 150 dakikadan büyük olanlarına ait kaç farklı replacement_cost değeri vardır?
 select count(distinct(replacement_cost)) from film where length > 150
+--film tablosunda bulunan filmleri rating değerlerine göre gruplayınız.
+select rating from film group by rating
+--film tablosunda bulunan filmleri replacement_cost sütununa göre grupladığımızda film sayısı 50 den fazla olan replacement_cost değerini ve karşılık gelen film sayısını sıralayınız.
+select replacement_cost, count(*) from  film group by replacement_cost having count(*) > 50 
+--customer tablosunda bulunan store_id değerlerine karşılık gelen müşteri sayılarını nelerdir? 
+select count(*),store_id from customer group by store_id
+--city tablosunda bulunan şehir verilerini country_id sütununa göre gruplandırdıktan sonra en fazla şehir sayısı barındıran country_id bilgisini ve şehir sayısını paylaşınız.
+SELECT country_id, COUNT(city) FROM city GROUP BY country_id ORDER BY COUNT(city) DESC LIMIT 1;
